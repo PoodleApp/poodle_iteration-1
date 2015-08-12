0c2c141c08fbc49f49606599548eaca78aba071c
Jesse Hallett <jesse@sitr.us>
2015-08-11 17:21:16 -0700

    simple Electron app

---
+++ main.js
@@ -0,0 +1,27 @@
+/* @flow */
+
+var app = require('app')
+var BrowserWindow = require('browser-window')
+
+require('crash-reporter').start()
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is GCed.
+var mainWindow = null
+
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow.loadUrl('file:///'+ __dirname +'/index.html')
+  mainWindow.openDevTools()
+  mainWindow.on('closed', function() {
+    mainWindow = null
+  })
+})
