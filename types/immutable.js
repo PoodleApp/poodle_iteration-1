6c2409d2523d804df15d6e34bada1a773d7f3ea1
Jesse Hallett <jesse@sitr.us>
2015-08-15 16:35:04 -0700

    Experimenting with type definitions for 'immutable'

---
+++ types/immutable.js
@@ -0,0 +1,13 @@
+
+declare class Immutable$Record<T:Object> {
+  get<A>(key: $Enum<T>): A;
+  set<A>(key: $Enum<T>, value: A): void;
+  remove(key: $Enum<T>): void;
+  size: number;
+}
+
+declare module immutable {
+
+  declare function Record<T:Object, R: T & Immutable$Record<T>>(spec: T): Class<R>;
+
+}
